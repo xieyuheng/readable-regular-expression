e4eeecbfbfc2edd157d8ece3990fe0ac1d57fa5b
Xie Yuheng <xyheme@gmail.com>
2020-05-02 14:15:36 +0800

    - `rr.non_capturing_group`

--- src/combinations/group.ts
+++ src/combinations/group.ts
@@ -2,6 +2,10 @@ export function group(re: RegExp): RegExp {
   return new RegExp(`(${re.source})`)
 }
 
+export function non_capturing_group(re: RegExp): RegExp {
+  return new RegExp(`(?:${re.source})`)
+}
+
 export function named_group(name: string, re: RegExp): RegExp {
   return new RegExp(`(?<${name}>${re.source})`)
 }
